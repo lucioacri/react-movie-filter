55c9a6aae2d92358b18d396f0082e5470312eef6
lucioacri <lucioacri@gmail.com>
2025-05-27 13:58:20 +0200

    add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
 
 function App() {
+  const filmsArray = [
+    { title: "Inception", genre: "Fantascienza" },
+    { title: "Il Padrino", genre: "Thriller" },
+    { title: "Titanic", genre: "Romantico" },
+    { title: "Batman", genre: "Azione" },
+    { title: "Interstellar", genre: "Fantascienza" },
+    { title: "Pulp Fiction", genre: "Thriller" },
+  ];
+
   return (
     <main>
-      <h1>ciao</h1>
+      <div className="container">
+        <div className="row g-3">
+          {filmsArray.map((film) => (
+            <div className="col">
+              <div className="card">
+                <div className="card-title">{film.title}</div>
+                <div className="card-content">{film.genre}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
